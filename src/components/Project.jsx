8ae009d964c0aff38f6dd302496d08a9ca9e16a5
belal3door <belaledoor25@gmail.com>
2025-06-14 19:36:06 +0200

    add burger button

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -10,26 +10,25 @@ export function Project() {
           My Projects
         </h2>
       </ScrollAnimation>
-      <div className="grid gap-8 overflow-hidden grid-cols-3">
+      <div className="grid gap-8 overflow-hidden grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
         {/* Card 1 */}
         <ScrollAnimation animateIn="flipInX">
           <div className="p-8 bg-gray-900 rounded-xl flex flex-col h-full text-white hover:translate-y-[-5px] hover:bg-gradient-to-r hover:from-purple-500 hover:to-purple-700 transition duration-300">
             <header className="flex items-center justify-between mb-14 text-blue-500">
               <svg
-  width="50"
-  xmlns="http://www.w3.org/2000/svg"
-  role="img"
-  viewBox="0 0 24 24"
-  fill="none"
-  stroke="#A855F7"   // <-- هنا لون البنفسجي
-  strokeWidth="1"
-  strokeLinecap="round"
-  strokeLinejoin="round"
->
-  <title>Folder</title>
-  <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
-</svg>
-
+                width="50"
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#A855F7"
+                strokeWidth="1"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <title>Folder</title>
+                <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z" />
+              </svg>
               <div className="flex items-center gap-4">
                 <a
                   href="https://github.com/BilalEdoor/Address-Book.git"
@@ -82,20 +81,19 @@ export function Project() {
           <div className="p-8 bg-gray-900 rounded-xl flex flex-col h-full text-white hover:translate-y-[-5px] hover:bg-gradient-to-r hover:from-purple-500 hover:to-purple-700 transition duration-300">
             <header className="flex items-center justify-between mb-14 text-blue-500">
               <svg
-  width="50"
-  xmlns="http://www.w3.org/2000/svg"
-  role="img"
-  viewBox="0 0 24 24"
-  fill="none"
-  stroke="#A855F7"   // <-- هنا لون البنفسجي
-  strokeWidth="1"
-  strokeLinecap="round"
-  strokeLinejoin="round"
->
-  <title>Folder</title>
-  <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
-</svg>
-
+                width="50"
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#A855F7"
+                strokeWidth="1"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <title>Folder</title>
+                <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z" />
+              </svg>
               <div className="flex items-center gap-4">
                 <a
                   href="https://github.com/BilalEdoor/project---react-js-training-.git"
@@ -145,21 +143,20 @@ export function Project() {
         <ScrollAnimation animateIn="flipInX">
           <div className="p-8 bg-gray-900 rounded-xl flex flex-col h-full text-white hover:translate-y-[-5px] hover:bg-gradient-to-r hover:from-purple-500 hover:to-purple-700 transition duration-300">
             <header className="flex items-center justify-between mb-14 text-blue-500">
-             <svg
-  width="50"
-  xmlns="http://www.w3.org/2000/svg"
-  role="img"
-  viewBox="0 0 24 24"
-  fill="none"
-  stroke="#A855F7"   // <-- هنا لون البنفسجي
-  strokeWidth="1"
-  strokeLinecap="round"
-  strokeLinejoin="round"
->
-  <title>Folder</title>
-  <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
-</svg>
-
+              <svg
+                width="50"
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#A855F7"
+                strokeWidth="1"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <title>Folder</title>
+                <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z" />
+              </svg>
               <div className="flex items-center gap-4">
                 <a
                   href="https://github.com/BilalEdoor/smart-driving-.git"
@@ -212,21 +209,20 @@ export function Project() {
         <ScrollAnimation animateIn="flipInX">
           <div className="p-8 bg-gray-900 rounded-xl flex flex-col h-full text-white hover:translate-y-[-5px] hover:bg-gradient-to-r hover:from-purple-500 hover:to-purple-700 transition duration-300">
             <header className="flex items-center justify-between mb-14 text-blue-500">
-                      <svg
-  width="50"
-  xmlns="http://www.w3.org/2000/svg"
-  role="img"
-  viewBox="0 0 24 24"
-  fill="none"
-  stroke="#A855F7"   // <-- هنا لون البنفسجي
-  strokeWidth="1"
-  strokeLinecap="round"
-  strokeLinejoin="round"
->
-  <title>Folder</title>
-  <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
-</svg>
-
+              <svg
+                width="50"
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#A855F7"
+                strokeWidth="1"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <title>Folder</title>
+                <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z" />
+              </svg>
               <div className="flex items-center gap-4">
                 <a
                   href="https://github.com/BilalEdoor/weather-journal-app.git"
@@ -279,21 +275,20 @@ export function Project() {
         <ScrollAnimation animateIn="flipInX">
           <div className="p-8 bg-gray-900 rounded-xl flex flex-col h-full text-white hover:translate-y-[-5px] hover:bg-gradient-to-r hover:from-purple-500 hover:to-purple-700 transition duration-300">
             <header className="flex items-center justify-between mb-14 text-blue-500">
-             <svg
-  width="50"
-  xmlns="http://www.w3.org/2000/svg"
-  role="img"
-  viewBox="0 0 24 24"
-  fill="none"
-  stroke="#A855F7"   // <-- هنا لون البنفسجي
-  strokeWidth="1"
-  strokeLinecap="round"
-  strokeLinejoin="round"
->
-  <title>Folder</title>
-  <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
-</svg>
-
+              <svg
+                width="50"
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#A855F7"
+                strokeWidth="1"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <title>Folder</title>
+                <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z" />
+              </svg>
               <div className="flex items-center gap-4">
                 <a
                   href="https://github.com/BilalEdoor/FEND-Capstone-travel-app.git"
